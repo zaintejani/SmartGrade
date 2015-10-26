bf92a3845e4b562ddf97af785a09a1dcbf07e67b
Zain Tejani <zain.tejani@gmail.com>
2015-10-26 13:09:14 -0400

    Checklist and notes

---
+++ checklist.R
@@ -0,0 +1,21 @@
+### CHECKLIST
+
+## Basic plots: Total distribution and Time Sewries DONE (basic unweighted-ish)
+
+## Combine old and new data. Remove irrelevant columns(from new). Clean data column classes using apply() DONE
+
+## ggplot2 tutorial. Add regressor lines to scatter plots, distribution curves instead of blocky hists.
+
+## shiny package install + tutorial. Mess with selector options to replace a and b in p1 and m1 selections.
+
+## Combine selction filers using && (relearn &&)
+
+## Investigate ML (regression) models to predict next semester GPA, with categorical considerations for semester.
+
+## Move up timeline, first shiny app live mid-Nov.
+
+
+
+## Ideas (random)
+
+#  b-style df for courses, filtered by major "handle" vector. "Cascading" scatter plots by major.
