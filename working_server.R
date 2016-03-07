fc257994525424eece12e8a5cb7325a448a745bf
Zain Tejani <zain.tejani@gmail.com>
2016-03-07 08:35:33 -0500

    app updates

--- working_server.R
+++ working_server.R
@@ -9,10 +9,37 @@ load("./Grade_data/final.RData")
 all[,2]<-as.numeric(all[,2])
 all[,3:4]<-apply(all[,3:4],2,function(x) as.character(x))
 
-f1<-as.data.frame(table(all$Field));f1<-f1[order(f1$Freq, decreasing=TRUE),]f1$Var1<-as.character(f1$Var1)
+f1<-as.data.frame(table(all$Field));f1$Var1<-as.character(f1$Var1)
+## f1<-f1[order(f1$Freq, decreasing=TRUE),]
 
 shinyServer(
   function(input, output){
+    
+    c1<-reactive(levels(as.factor(all[all$Field==input$field,6])))
+    
+    output$course1 <- renderUI({
+      selectInput(inputId="course", label="Course", choices=c1())
+    })
+    
+    pr1<-reactive({
+      levels(as.factor(all[all$Field==input$field && all$Course==input$course,all$Name]))
+    })
+    
+    output$p1<-renderUI({
+      selectInput(inputId="prof1", label="Professor 1", choices=pr1())
+    })
+    
+    pr2<-reactive(pr1()[pr1()!=input$prof1])
+    
+    output$p2<-renderUI({
+      selectInput(inputId="prof2", label="Professor 2", choices=pr2())
+    })
+    
+    plotDF<-reactive({
+      all[all$Level==input$level & all$Field==input$field & all$Course==input$course & all$Name %in% c(input$prof1,input$prof2) & all$Semester %in% input$semChoice & all$Term.Yr %in% input$yearChoice & all$Class.Size %in% input$sizeFilter,]
+    })
+    
+    ## insert if statement for plot type here
     
   }
 )
--- working_ui.R
+++ working_ui.R
@@ -10,17 +10,17 @@ shinyUI(fluidPage(
     sidebarPanel(
       h3("Select Field and Professors"),
       
-      p("Use the tools below to compare porfessors teaching the same course"),
+      p("Use the tools below to compare professors teaching the same course"),
       
       selectInput(inputId="level", label="Level of Study", choices=c("Undergraduate","Graduate"), selected="Undergraduate"),
       
       selectInput(inputId="field", label="Field of Study", choices=f1$Var1),
       
-      selectInput(inputId="course", label="Course", choices=),
+      uiOutput("course1"),
       
-      selectInput(inputId="prof1", label="Professor 1", choices=),
+      uiOutput("p1"),
       
-      selectInput(inputId="prof2", label="Professor 2", choices=),
+      uiOutput("p2"),
       
       checkboxGroupInput(inputId="semChoice", label="Semesters Selected",
                          choices=c("Fall","Spring","Summer"), selected=c("Fall","Spring","Summer")),
@@ -29,10 +29,12 @@ shinyUI(fluidPage(
                   max=max(all$Term.Yr), value=c(min(all$Term.Yr), max(all$Term.Yr)), rounding=TRUE),
       
       checkboxGroupInput(inputId="sizeFilter", label="Class Size",
-                         choices=levels(as.factor(all$Class.Size)), selected=levels(as.factor(all$Class.Size)))
+                         choices=levels(as.factor(all$Class.Size)), selected=levels(as.factor(all$Class.Size))),
       
       radioButtons(inputId="plotType", label="Plot Type",
-                   choices=c("Head-to-head aggregate weighted grades","Head-to-head average GPA over time"))
+                   choices=list("Prof 1 aggregate weighted grades"=1,"Prof 2 aggregate weighted grades"=2,
+                             "Prof 1 average GPA over time"=3,"Prof 2 average GPA over time"=4,
+                             "Head-to-head aggregate weighted grades"=5,"Head-to-head average GPA over time"=6))
       
     )
     
